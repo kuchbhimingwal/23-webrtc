3cad78d677b992efc6c70b97f79a9035f29e62b1
kuchbhimingwal <shubhammingi@gmail.com>
2024-07-04 12:07:30 -0400

    created the ws server !

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
 
@@ -10,7 +10,23 @@ wss.on('connection', function connection(ws) {
 
   ws.on('message', function message(data: any) {
     const message = JSON.parse(data);
-    
+    if(message.type === "identify-as-sender"){
+      senderSocket = ws;
+    } else if (message.type === "identify-as-receiver"){
+      receiverSocket = ws;
+    } else if (message.type === "create-offer"){
+      receiverSocket?.send(JSON.stringify({type: "offer", offer: message.offer}))
+    } else if (message.type === "create-answere") {
+      senderSocket?.send(JSON.stringify({type: "answere", offer: message.offer}))
+    } else if (message.type === 'iceCandidate') {
+      if (ws === senderSocket) {
+        receiverSocket?.send(JSON.stringify({ type: 'iceCandidate', candidate: message.candidate }));
+      } else if (ws === receiverSocket) {
+        senderSocket?.send(JSON.stringify({ type: 'iceCandidate', candidate: message.candidate }));
+      }
+    }
+
+    console.log(message);
   });
 
   ws.send('something');
